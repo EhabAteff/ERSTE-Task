9a89a00cfeb41fdf345dd206a1a4eea0cc943a13
Ehab Mohamed <ihabatef@hotmail.com>
2023-01-24 23:50:14 +0200

    test: handling login modal

--- cypress/e2e/step_definitions/login.cy.js
+++ cypress/e2e/step_definitions/login.cy.js
@@ -1,5 +1,5 @@
 import { loginToWebsite, navigateToWebsite } from "../../support/login"
-const { Given, When, Then } = require("@badeball/cypress-cucumber-preprocessor")
+const { Given, Then } = require("@badeball/cypress-cucumber-preprocessor")
 
 Given('User navigates to the website', ()=> {
     navigateToWebsite()
@@ -8,6 +8,12 @@ Given('User logs-in successfully', ()=>{
   loginToWebsite()
 })
 
-Given('User is at the homepage Url', ()=>{
+Then('User is at the homepage Url', ()=>{
   cy.url().should("eq","https://george.csas.cz/index.html?country=CZ&at=c#/overview")
+  cy.get('.modal-header > .g-button')
+  .then($body => {
+    if ($body.find('.g-button-label').length) {
+      cy.get('.modal-header > .g-button').click()
+    }
+  })
 })
